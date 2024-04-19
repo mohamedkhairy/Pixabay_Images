0a1c94a0be2b0ba94c9d0b288ee62290bbff6353
Mohamed Khairy <mohamed.khairy@iqraaly.com>
2024-04-19 23:36:52 +0200

    prepare shared UI module

---
+++ core/ui/src/main/java/com/example/core/ui/component/PixabayAlertDialog.kt
@@ -0,0 +1,52 @@
+package com.example.core.ui.component
+
+import android.util.Log
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PixabayInfoDialog(
+    goToImageDetails: () -> Unit,
+    onCancel: () -> Unit,
+    visibility: Boolean = true
+) {
+    var isDialogVisible by remember {mutableStateOf(visibility)}
+
+
+//    stringResource(R.string.congratulations)
+    if (isDialogVisible) {
+        AlertDialog(
+            onDismissRequest = {},
+            title = { Text(text = "\uD83D\uDC4B") },
+            text = { Text(text = "We are about to open the details!") },
+            modifier = Modifier.wrapContentWidth(),
+            dismissButton = {
+                TextButton(onClick = {
+                    onCancel()
+                    Log.d("xxx", "Cancel -> ${isDialogVisible}")
+//                    isDialogVisible = false
+                }) {
+                    Text(text = "Cancel")
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        goToImageDetails()
+//                        isDialogVisible = false
+                    }
+                ) {
+                    Text(text = "Open")
+                }
+            }
+        )
+    }
+}
